75af99b7f832320fcdf02235ce523a3b468c812e
Always Hungry <shalini.bani@gmail.com>
2026-07-23 18:57:52 +0200

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.go
@@ -0,0 +1,25 @@
+import "slices"
+func minEatingSpeed(piles []int, h int) int {
+   left, right := 1, slices.Max(piles)
+   minK := right
+   for left <= right{
+	mid := left + (right -left)/2
+	currh := 0
+	for _, p := range piles{
+		if currh > h{
+			break
+		}
+
+		currh += (p+mid-1)/mid
+	}
+
+	if currh <= h{
+		minK = min(minK, mid) 
+		right = mid - 1
+	}else{
+		left = mid + 1
+	}
+   }
+
+   return minK
+}

de9516a1d560d450b7d40930b5ed479e7c32e903
Always Hungry <shalini.bani@gmail.com>
2026-09-14 18:48:55 +0200

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.go
@@ -0,0 +1,52 @@
+type MaxHeap []int
+
+func (h *MaxHeap) Push(x any){
+   *h = append(*h, x.(int))
+}
+
+func (h *MaxHeap) Pop() any{
+    n := len(*h)
+    old := *h
+
+    x := old[n-1]
+    *h = old[:n-1]
+
+    return x
+}
+
+func (h MaxHeap) Swap(i,j int) {
+    h[i], h[j] = h[j], h[i]
+}
+
+func (h MaxHeap) Less(i,j int) bool{
+    return h[i] > h[j]
+}
+
+func (h MaxHeap) Len() int{
+    return len(h)
+}
+
+func lastStoneWeight(stones []int) int {
+    h := &MaxHeap{}
+
+    heap.Init(h)
+
+    for _, s := range stones{
+       heap.Push(h, s)
+    }
+
+    for h.Len() > 1{
+        s1 := heap.Pop(h).(int)
+        s2 := heap.Pop(h).(int)
+
+        if s1!=s2{
+            heap.Push(h, s1-s2)
+        }
+    }
+
+    if h.Len() > 0{
+        return heap.Pop(h).(int)
+    }
+
+    return 0
+}

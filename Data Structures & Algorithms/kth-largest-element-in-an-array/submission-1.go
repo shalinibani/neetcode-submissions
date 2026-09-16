62d293ce56734c5839e720df96fee3d387ce78b4
Always Hungry <shalini.bani@gmail.com>
2026-09-16 22:09:05 +0200

    Add: kth-largest-element-in-an-array - submission-1

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-1.go
@@ -0,0 +1,39 @@
+type MinHeap []int
+
+func (h *MinHeap) Push(x any){
+	*h = append(*h, x.(int))
+}
+
+func (h *MinHeap) Pop()any{
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+
+	return x
+}
+
+func (h MinHeap) Len() int{
+	return len(h)
+}
+
+func (h MinHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h MinHeap) Less(i, j int) bool{
+	return h[i] < h[j]
+}
+
+func findKthLargest(nums []int, k int) int {
+  h := &MinHeap{}
+
+  for _, n := range nums{
+	heap.Push(h, n)
+	if h.Len() > k{
+		heap.Pop(h)
+	}
+  }
+
+  return (*h)[0]
+}

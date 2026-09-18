1d61b5e9d9d67da2495c657c7d7fcd0e32d2debf
Always Hungry <shalini.bani@gmail.com>
2026-09-18 14:14:03 +0200

    Add: lowest-common-ancestor-in-binary-search-tree - submission-0

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-0.go
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func lowestCommonAncestor(root *TreeNode, p *TreeNode, q *TreeNode) *TreeNode {
+    if root == nil{
+		return nil
+	}
+
+	if p.Val < root.Val && root.Val < q.Val{
+		return root
+	}
+
+	if p.Val < root.Val && q.Val < root.Val{
+		return lowestCommonAncestor(root.Left, p, q)
+	} 
+	
+	if p.Val > root.Val && q.Val > root.Val{
+		return lowestCommonAncestor(root.Right, p, q)
+	}
+
+	return root
+
+}
